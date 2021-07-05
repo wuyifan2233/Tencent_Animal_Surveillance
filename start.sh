b617b725b13af44c304bc8687dfb0cd2960c412f
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 19:29:31 +0800

    udpate on train config

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
---batch 32 \
---epochs 102 \
---img-size
+--batch 64 \
+--epochs 120 \
+--img-size 1280 \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
 --name b32-ep120-s6 \
