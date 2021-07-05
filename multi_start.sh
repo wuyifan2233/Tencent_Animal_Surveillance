0d7898249284fa251986062ea33e7a083117c38a
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 20:23:16 +0800

    udpate

--- multi_start.sh
+++ multi_start.sh
@@ -2,11 +2,10 @@
 python -m torch.distributed.launch --nproc_per_node 8 train.py \
 --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
 --batch 128 \
---epochs 120 \
---data ./Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
+--epochs 2 \
+--img-size 640 \
+--data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name multi-b128-ep120-1231-5s6 \
---project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
---hyp ./Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
---sync-bn \
---device 0,1,2,3,4,5,6,7 \
+--name multi-test \
+--project /apdcephfs/private_luxiferchen/Tencent_WWF/multi_training_projects/ \
+--hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
---batch 16 \
+--batch 8 \
 --epochs 120 \
 --img-size 640 \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name b16-ep120-img640-s \
+--name b8-ep120-img640-s \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
