a71c9c9a708a4b03e99beb15c79009bac5ea369c
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 11:20:39 +0800

    update json

--- train_start.sh
+++ train_start.sh
@@ -1,7 +1,7 @@
 # wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
 --batch 32 \
---epochs 120 \
+--epochs 2 \
 --data ./Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
 --name single-b32-ep120-1231-5s6 \
