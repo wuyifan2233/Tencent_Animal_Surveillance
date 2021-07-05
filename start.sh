43ccfcf11f8565210e1bbcd32fb40f3efe21695f
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 19:48:40 +0800

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
---batch 64 \
---epochs 120 \
+--batch 16 \
+--epochs 2 \
 --img-size 1280 \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name b32-ep120-s6 \
+--name single-test-imagesize1280 \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
