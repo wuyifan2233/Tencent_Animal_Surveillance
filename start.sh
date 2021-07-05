c5fb739bf41420149071f5e023accaafdf1802ec
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 20:04:12 +0800

    test

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
---batch 32 \
---epochs 2 \
+python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
+--batch 128 \
+--epochs 120 \
 --img-size 640 \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name single-test-imagesize1280 \
+--name b128-ep120-img640-s \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
