df9cc8053f72187809472d09ff0caa4eca8d75ac
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 20:15:53 +0800

    udpate

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
---batch 128 \
+--batch 64 \
 --epochs 120 \
 --img-size 640 \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name b128-ep120-img640-s \
+--name b64-ep120-img640-s \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
