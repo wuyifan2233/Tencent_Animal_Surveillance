800ae51fbf0c264853f7712f22c3e32e24e90994
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-05 10:48:44 +0800

    add config files

--- train_start.sh
+++ train_start.sh
@@ -2,9 +2,8 @@
 python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
 --batch 32 \
 --epochs 120 \
---img-size 1280 \
 --data ./Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
 --name single-b32-ep120-1231-5s6 \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
---hyp ./Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
+--hyp ./Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml 
