12619461c29d511caa6a95695e50965bbc5d1c91
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-06 18:47:54 +0800

    upade

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d;
-python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
---batch 64 \
---epochs 120 \
---img-size 640 \
---data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
---cache \
+python train.py \
+--data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1.yaml \
+--hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
+--cfg /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/models/yolov5s.yaml \
+--project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/top23-p1234-p1/ \
 --name b64-ep120-img640-s-wandb \
---project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
---hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml
+--img-size 640 \
+--epochs 120 \
+--batch 64 \
+--cache \
+
