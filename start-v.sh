99197f71a723e063d700b58d887ccaa6b764fa12
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-07 13:01:21 +0800

    update

--- start-v.sh
+++ start-v.sh
@@ -1,13 +1,13 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d;
 python train.py \
---data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1.yaml \
+--data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1-v.yaml \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
 --cfg /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/models/yolov5s.yaml \
---project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/top23-p1234-p1/ \
---name b32-ep120-img640-s-wandb \
+--project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/top23-p1234-p1-v/ \
+--name b64-ep120-img640-s-wandb-v \
 --img-size 640 \
 --weights '' \
 --epochs 120 \
---batch 32 \
---cache \ > /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/log_txt/.txt
+--batch 64 \
+--cache \ > /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/log_txt/b64-ep120-img640-s-wandb-v.txt
 
