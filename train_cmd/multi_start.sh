dbb0123039bb7cc414e10b070020f353b2454943
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-07 20:58:14 +0800

    update

--- train_cmd/multi_start.sh
+++ train_cmd/multi_start.sh
@@ -1,6 +1,6 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
 python -m torch.distributed.launch --nproc_per_node 8 train.py \
---data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1.yaml \
+--data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1-v.yaml \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch-p6.yaml \
 --cfg /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/models/hub/yolov5s6.yaml \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/p1234-p1-multi/ \
@@ -9,4 +9,4 @@ python -m torch.distributed.launch --nproc_per_node 8 train.py \
 --weights '' \
 --epochs 5 \
 --batch 64 \
---cache \ > /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/log_txt/b64-ep120-img640-s-wandb-notqdm.txt
+--cache \ > /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/log_txt/multi-test.txt
