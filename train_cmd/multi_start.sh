4872852c4868ef5b9dccb7964aa80d397fa7e534
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-07 21:52:18 +0800

    update

--- train_cmd/multi_start.sh
+++ train_cmd/multi_start.sh
@@ -1,5 +1,5 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
-python -m torch.distributed.launch --nproc_per_node 8 ../train.py \
+python -m torch.distributed.launch --nproc_per_node 8 train.py \
 --data /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/configs/top23-p1234-p1-v.yaml \
 --hyp /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/data/hyps/hyp.scratch-p6.yaml \
 --cfg /apdcephfs/private_luxiferchen/Tencent_WWF/Tencent_Animal_Surveillance/models/hub/yolov5s6.yaml \
